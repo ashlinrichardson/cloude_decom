0f9b0ca23ba7f108bf23b59b965b753791d92298
ashlinrichardson <richardson.ashlin@gmail.com>
2021-08-22 22:47:20 -0700

    update

--- cpp/compile.py
+++ cpp/compile.py
@@ -12,13 +12,17 @@ def run(cmd):
     return os.system(cmd)
 
 # ubuntu 20 dependencies
-run('sudo apt install g++ freeglut3-dev')
+if len(os.popen('g++').read().strip().split('fatal')) < 1:
+    run('sudo apt install g++')
+if not os.path.exists('/usr/include/GL/glut.h'):
+    run('sudo apt install freeglut3-dev')
 
 def build(n):
-    run('rm -f ' + n) 
-    run('./make_' + n + '.sh')
-    run('chmod 755 ' + n)
-    run('sudo mv ' + n + ' /usr/bin/' + n)
+    run('rm -f cpp/' + n) 
+    run('cd cpp; ./make_' + n + '.sh')
+    run('chmod 755 cpp/' + n)
+    run('sudo mv cpp/' + n + ' /usr/bin/' + n)
 
+# build both programs
 for n in ['cloude_decom', 'cloude_view']:
     build(n)
