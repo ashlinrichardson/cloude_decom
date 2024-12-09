68444a2c90f8fcf30baa77233a7c5d43929888d4
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-12-09 15:00:12 -0800

    update

--- py/misc.py
+++ py/misc.py
@@ -127,3 +127,16 @@ if __name__ == '__main__':
         err('unexpected number of bands')
 
     print('test passed')
+
+
+def parfor(my_function,  # function to run in parallel
+           my_inputs,  # inputs evaluated by worker pool
+           n_thread=mp.cpu_count()): # cpu threads to use
+
+    if n_thread == 1:  # don't use multiprocessing for 1-thread
+        return [my_function(my_inputs[i])
+                for i in range(len(my_inputs))]
+    else:
+        n_thread = (mp.cpu_count() if n_thread is None
+                    else n_thread)
+        return mp.Pool(n_thread).map(my_function, my_inputs)
